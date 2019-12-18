ce91486c05738e63b8dc79c021bfa1f8b021027e
Roman00 <rvasin00@gmail.com>
2019-12-18 22:32:06 +0600

    feat: add jquery cdn & button scroll up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -10,4 +10,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+});*/
+
+// jquery code
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible');
+  });
+
+  var btn = $("#upbtn");
+    $(window).scroll(function(){
+      $(this).scrollTop() < 2000 ? btn.hide() : btn.show();  
+    });  
+    btn.click(function(){
+      $("html,body").animate({scrollTop:0},600);
+    });
 });
