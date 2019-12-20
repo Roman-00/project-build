76334a758858aaf4847c325c461d88e862624bdf
Roman00 <rvasin00@gmail.com>
2019-12-20 22:22:54 +0600

    fixed: swiper slider

--- js/main.js
+++ js/main.js
@@ -52,11 +52,11 @@ $(document).ready(function () {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
-      type: 'bullets',
+      type: 'bullets'
     },
     navigation: {
-      nextE1: '.swiper-button-next',
-      prevE1: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 
@@ -64,7 +64,6 @@ $(document).ready(function () {
   var prev = $('.swiper-button-prev');
   var bullets = $('.swiper-pagination');
 
-  next.css('left', prev.width() + 10 + bullets.width() + 10)
-  bullets.css('left', prev.width() + 10)
-
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10);
 });
