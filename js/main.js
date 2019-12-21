231ced03ef65955e45d715a7c2ae7c78b9c399be
Roman00 <rvasin00@gmail.com>
2019-12-21 15:24:57 +0600

    feat: add animate css and wow js

--- js/main.js
+++ js/main.js
@@ -66,4 +66,17 @@ $(document).ready(function () {
 
   next.css('left', prev.width() + 10 + bullets.width() + 10);
   bullets.css('left', prev.width() + 10);
+
+  //animate wow 
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       250,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+    }
+  );
+  wow.init();
+
 });
