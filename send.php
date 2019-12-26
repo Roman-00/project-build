fe842e230fbe8d4cb6ec8ae200d19eb8de1bd18c
Roman00 <rvasin00@gmail.com>
2019-12-26 23:52:09 +0600

    fixed: php files php mailer

--- send.php
+++ send.php
@@ -14,7 +14,7 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
     //Server settings
-    $mail -> charSet = "utf-8";
+    $mail->CharSet = 'UTF-8';
     $mail->SMTPDebug = 2;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
@@ -38,3 +38,5 @@ try {
 } catch (Exception $e) {
     echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
 }
+
+?>
